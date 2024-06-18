0c4e3bc71f84a9a781c36eb755386f308f54470b
Edwin Park <esp1@cornell.edu>
2024-06-18 11:57:35 -0700

    don't use site-defaults

--- src/clj/todo_list/handler.clj
+++ src/clj/todo_list/handler.clj
@@ -5,8 +5,8 @@
             [ring.util.response :as response]))
 
 (defroutes app-routes
-   (route/not-found "Not Found"))
+  (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes (assoc site-defaults :static {:resources "public"
-                                                             :files     "resources/public"})))
+  (wrap-defaults app-routes {:static {:resources "public"
+                                      :files     "resources/public"}}))
