0bd84e9d5b80b8b9c23407551bea662080905016
rfpg <rickygraham@gwu.edu>
2024-06-19 14:44:33 -0400

    working drag and drop

--- src/clj/todo_list/handler.clj
+++ src/clj/todo_list/handler.clj
@@ -5,18 +5,21 @@
             [ring.util.response :as response]
             [selmer.parser :as selmer]))
 
-(def state (atom [{:id "1718739873776", :name "moo", :status :to-do}]))
+(defonce todo-list-atom (atom [{:id "1718739873776", :name "moo", :status :to-do}]))
 
-(defn add-task [state n]
-  (swap! state conj {:id (str (System/currentTimeMillis))
-                     :name n
-                     :status :to-do}))
 
-(defn get-tasks [state status]
+(defn add-task 
+  "Adds a new task with the name n to the state atom." 
+  [state-atom n]
+  (swap! state-atom conj {:id (str (System/currentTimeMillis))
+                          :name n
+                          :status :to-do}))
+
+(defn get-tasks [state-atom status]
   (filter (fn [task]
             (= (:status task)
                status))
-          @state))
+          @state-atom))
 
 (defn set-task-status [state task-id status]
   (map (fn [task]
@@ -24,6 +27,18 @@
            (assoc task :status status)
            task))
        state))
+
+; this is making the status change; it returns a new state object with status changes 
+(defn bulk-change-status [state from-status to-status]
+  (map (fn [task]
+         (if (= (:status task) from-status)
+           (assoc task :status to-status)
+           task))
+       state))
+
+; this is applying the change / swap; calling function above (making status changes) and updating the value of atom
+(defn bulk-change-status! [state-atom from-status to-status]
+  (swap! state-atom bulk-change-status from-status to-status))
 
 (defn render-task [task]
   (selmer/render
@@ -36,12 +51,12 @@
     {:status 200
      :headers {"Content-Type" "text/html"},
      :body (str
-            "<div id='to-do-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :to-do))) "</div>"
-            "<div id='in-progress-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :in-progress))) "</div>"
-            "<div id='done-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :done))) "</div>")})
+            "<div id='to-do-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks todo-list-atom :to-do))) "</div>"
+            "<div id='in-progress-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks todo-list-atom :in-progress))) "</div>"
+            "<div id='done-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks todo-list-atom :done))) "</div>")})
 
   (POST "/task/:status/:id" [status id]
-    (swap! state set-task-status id (keyword status))
+    (swap! todo-list-atom set-task-status id (keyword status))
     {:status 204})
 
   (route/not-found "Not Found"))
@@ -49,3 +64,13 @@
 (def app
   (wrap-defaults app-routes {:static {:resources "public"
                                       :files     "resources/public"}}))
+
+(comment
+  (add-task todo-list-atom "Final project")
+  (add-task todo-list-atom "Exam") 
+  (add-task todo-list-atom "Quiz") 
+  @todo-list-atom
+  (bulk-change-status! todo-list-atom :in-progress :done)
+  
+  )
+
