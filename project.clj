51fd6b4a4a766bac975e77ce9db00f34a3a7c1d6
Edwin Park <esp1@cornell.edu>
2024-06-18 19:46:46 -0700

    functioning drag and drop

--- project.clj
+++ project.clj
@@ -2,12 +2,14 @@
   :description "TODO with Clojure"
   :dependencies [[org.clojure/clojure "1.11.3"]
                  [compojure "1.6.2"]
-                 [hiccup "2.0.0-RC3"]
                  [ring/ring-defaults "0.5.0"]
                  [ring/ring-core "1.12.1"]
-                 [ring/ring-jetty-adapter "1.12.1"]]
-  :plugins [[lein-ring "0.12.6"]]
+                 [ring/ring-jetty-adapter "1.12.1"]
+                 [selmer "1.12.61"]]
+  :plugins [[cider/cider-nrepl "0.49.0"]
+            [lein-ring "0.12.6"]]
   :source-paths ["src/clj"]
   :resource-paths ["resources"]
   :clean-targets ^{:protect false} ["target"]
-  :ring {:handler todo-list.handler/app})
+  :ring {:handler todo-list.handler/app
+         :nrepl {:start? true}})
--- src/clj/todo_list/handler.clj
+++ src/clj/todo_list/handler.clj
@@ -1,12 +1,49 @@
 (ns todo-list.handler
   (:require [compojure.core :refer [defroutes GET POST PUT DELETE]]
             [compojure.route :as route]
-            [hiccup2.core :as h]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.util.response :as response]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.util.response :as response]
+            [selmer.parser :as selmer]))
+
+(def state (atom [{:id "1718739873776", :name "moo", :status :to-do}]))
+
+(defn add-task [state n]
+  (swap! state conj {:id (str (System/currentTimeMillis))
+                     :name n
+                     :status :to-do}))
+
+(defn get-tasks [state status]
+  (filter (fn [task]
+            (= (:status task)
+               status))
+          @state))
+
+(defn set-task-status [state task-id status]
+  (map (fn [task]
+         (if (= (:id task) task-id)
+           (assoc task :status status)
+           task))
+       state))
+
+(defn render-task [task]
+  (selmer/render
+   "<div class='task' draggable='true' ondragstart='drag(event)' id='{{id}}'>{{name}}</div>"
+   {:id (:id task)
+    :name (:name task)}))
 
 (defroutes app-routes
-  (GET "/foo" []{:status 200, :headers {"Content-Type" "text/html"}, :body (str (h/html [:span {:class "foo"} "bar"]))})
+  (GET "/state" []
+    {:status 200
+     :headers {"Content-Type" "text/html"},
+     :body (str
+            "<div id='to-do-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :to-do))) "</div>"
+            "<div id='in-progress-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :in-progress))) "</div>"
+            "<div id='done-tasks' hx-swap-oob='innerHTML'>" (apply str (map render-task (get-tasks state :done))) "</div>")})
+
+  (POST "/task/:status/:id" [status id]
+    (swap! state set-task-status id (keyword status))
+    {:status 204})
+
   (route/not-found "Not Found"))
 
 (def app
